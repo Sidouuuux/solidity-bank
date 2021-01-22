7de2ae6d0967c99a4b5294265bed8788d44bb2e7
Sidouuuux <sidoux@pop-os.localdomain>
2021-01-22 13:03:24 +0100

    ajout du smart contract ERC20 mais non utilisé

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -27,3 +27,35 @@ contract SimpleBank {
         return address(this).balance;
     }
 }
+
+contract MyToken {
+    string private name;
+    string private symbol;
+    uint256 private totalSupply;
+    string private owner;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) public {
+        balanceOf[msg.sender] = _totalSupply;
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+    }
+    
+    function initBalance(uint256 _value) public returns (bool success){
+        balanceOf[msg.sender] -= _value;
+        return true;
+    }
+    
+    function getBalance() public view returns (uint256 balance){
+        return balanceOf[msg.sender];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value, "Your balance is too low");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        return true;
+    }
+}
